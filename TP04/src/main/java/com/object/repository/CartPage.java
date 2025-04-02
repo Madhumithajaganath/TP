1cdddef3adb54f14e3c7d8a92b8825258380540f
Madhumithajaganath <90049051+Madhumithajaganath@users.noreply.github.com>
2025-04-02 11:43:49 +0530

    Update CartPage.java

--- TP04/src/main/java/com/object/repository/CartPage.java
+++ TP04/src/main/java/com/object/repository/CartPage.java
@@ -12,16 +12,17 @@ public class CartPage {
 	
 	public CartPage(AndroidDriver driver) {
 		this.driver=driver;
-		PageFactory.initElements(driver,this);	
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+
 	}
 	
-	@FindBy(id = "com.androidsample.generalstore:id/appbar_btn_back")
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/appbar_btn_back")
 	private WebElement backBtn;
 	
-	@FindBy(className =  "android.widget.CheckBox")
+	@AndroidFindBy(className =  "android.widget.CheckBox")
 	private WebElement checkBox;
 	
-	@FindBy(id = "com.androidsample.generalstore:id/btnProceed")
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/btnProceed")
 	private WebElement visitWebsiteBtn;
 	
 	public WebElement getBackBtn() {
